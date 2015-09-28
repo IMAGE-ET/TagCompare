e16d83ff89b3250af9c3e22c14246aa8d778b242
Ryan Lee <ryan.me@live.com>
2015-09-28 22:47:34 +0800

    New Test Project

---
+++ TagCompare/TagCompare/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TagCompare
+//
+//  Created by Ryan on 15/9/28.
+//  Copyright © 2015年 Ryan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
